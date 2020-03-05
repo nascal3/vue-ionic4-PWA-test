1bd74ae1587ffd39bcc6ccd2cce7f1d5b6fcf2ca
nascal3 <nascal3@gmail.com>
2020-03-05 17:45:04 +0300

    Created zip code resolver

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import IonicVue from '@ionic/vue';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
 
+Vue.use(IonicVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
